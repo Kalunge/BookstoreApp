1a6ad5cb174b36c9a737e8aea53ba6c1ad4d40bf
kalunge <muthomititus+github@gmail.com>
2022-03-30 19:14:16 +0300

    Remove commented code to make codebase cleaner

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,23 +1,18 @@
 class BooksController < ApplicationController
   before_action :set_book, only: %i[show edit update destroy]
 
-  # GET /books or /books.json
   def index
     @books = Book.all
   end
 
-  # GET /books/1 or /books/1.json
   def show; end
 
-  # GET /books/new
   def new
     @book = Book.new
   end
 
-  # GET /books/1/edit
   def edit; end
 
-  # POST /books or /books.json
   def create
     @book = Book.new(book_params)
 
@@ -32,7 +27,6 @@ class BooksController < ApplicationController
     end
   end
 
-  # PATCH/PUT /books/1 or /books/1.json
   def update
     respond_to do |format|
       if @book.update(book_params)
@@ -45,7 +39,6 @@ class BooksController < ApplicationController
     end
   end
 
-  # DELETE /books/1 or /books/1.json
   def destroy
     @book.destroy
 
@@ -57,12 +50,10 @@ class BooksController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_book
     @book = Book.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def book_params
     params.require(:book).permit(:title, :category, :description)
   end
