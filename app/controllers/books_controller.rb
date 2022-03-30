0dd6bb73b4553cdbaa054bfb313fb96058efb8c6
kalunge <muthomititus+github@gmail.com>
2022-03-30 18:29:34 +0300

    Add a description column to book model

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -65,6 +65,6 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      params.require(:book).permit(:title, :category)
+      params.require(:book).permit(:title, :category, :description)
     end
 end
